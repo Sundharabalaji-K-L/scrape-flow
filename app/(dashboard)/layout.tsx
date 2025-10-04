94ec67c58b9b1d6bf866c0020f714703ccdb285f
Sundharabalaji-K-L <narasimhanbalaji13@gmail.com>
2025-10-04 19:18:18 +0530

    added breadcrumbs

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Separator} from "@/components/ui/separator";
 import DesktopSidebar from "@/components/Sidebar";
+import BreadcrumbHeader from "@/components/BreadcrumbHeader";
 
 const layout = ({children}: {children: React.ReactNode}) => {
     return (
@@ -9,7 +10,7 @@ const layout = ({children}: {children: React.ReactNode}) => {
             <div className="flex flex-col flex-1 min-h-screen">
                 <header className="flex  items-center justify-between px-6 py-4
                 h-[50px] container">
-                    Scrape Flow
+                   <BreadcrumbHeader />
                 </header>
                 <Separator/>
                 <div className="overflow-auto">
---
+++ components/BreadcrumbHeader.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from 'react';
+import {usePathname} from "next/navigation";
+import {
+    Breadcrumb,
+    BreadcrumbLink,
+    BreadcrumbItem,
+    BreadcrumbList,
+    BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+
+const BreadcrumbHeader = () => {
+    const pathName = usePathname();
+    const paths = pathName === "/" ? [""] : pathName?.split("/");
+
+    return (
+        <div className="flex items-center flex-start">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    {paths.map((path, index)=> (
+                        <React.Fragment key={index}>
+                            <BreadcrumbItem>
+                                <BreadcrumbLink
+                                    className="capitalize"
+                                    href={`/${path}`}
+                                >
+                                    {path === "" ? "home" : path}
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                        </React.Fragment>
+                    ))}
+                </BreadcrumbList>
+            </Breadcrumb>
+        </div>
+    );
+};
+
+export default BreadcrumbHeader;
