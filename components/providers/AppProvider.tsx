841b290de3314e79ff3587ddd102d5387e1f7940
Sundharabalaji-K-L <narasimhanbalaji13@gmail.com>
2025-10-06 00:17:05 +0530

    added workflow dialog

--- components/providers/AppProvider.tsx
+++ components/providers/AppProvider.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 
-import React from "react";
+import React, {useState} from "react";
 import {ThemeProvider} from "next-themes";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 export function AppProviders({children}: {children: React.ReactNode}) {
-    return <ThemeProvider attribute="class"
-    defaultTheme="system" enableSystem>
-        {children}
-    </ThemeProvider>
+    const [queryClient] = useState(() => new QueryClient());
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider attribute="class"
+                           defaultTheme="system" enableSystem>
+                {children}
+            </ThemeProvider>
+            <ReactQueryDevtools />
+        </QueryClientProvider>
+    )
 }
