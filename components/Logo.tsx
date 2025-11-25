50680b6cb422e64bcbd2d6d3521e44a2ed56485f
Sundharabalaji-K-L <narasimhanbalaji13@gmail.com>
2025-11-25 12:38:55 +0530

    initial commit

---
+++ components/Logo.tsx
@@ -0,0 +1,38 @@
+import { cn } from '@/lib/utils';
+import { SquareMousePointer } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react'
+
+function Logo({
+  fontSize="2xl", iconSize=20
+}: {
+  fontSize?: string,
+  iconSize?: number
+}) {
+  return (
+    <Link href="/" className={cn(
+      "text-2xl font-extrabold flex items-center gap-2",
+      fontSize
+    )}>
+
+      <div className="rounded-xl bg-gradient-to-r
+      from-rose-500 to-rose-600 p-2">
+        <SquareMousePointer size={iconSize} className="stroke-white"/>
+      </div>
+
+       
+      <div>
+        <span className="bg-gradient-to-r from-rose-500
+        to-rose-600 bg-clip-text text-transparent">
+          Flow
+        </span>
+        <span className="text-stone-700
+        dark:text-stone-300">Scrape</span>
+      </div>
+
+    </Link>
+
+  )
+}
+
+export default Logo;
